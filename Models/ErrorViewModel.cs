1f0ad9cd33e37c90996aaf7aa4418636c199ec3b
ThanhNam2601 <ltn262001@gmail.com>
2023-08-04 10:17:07 +0700

    LeThanhNam_Cau2

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LeThanhNam_03.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
