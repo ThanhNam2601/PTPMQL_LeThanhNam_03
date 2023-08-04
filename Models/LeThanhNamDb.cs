d3a88388983071c7657b24cae33ca13d909f0fea
ThanhNam2601 <ltn262001@gmail.com>
2023-08-04 10:55:01 +0700

    LeThanhNam_Cau3

---
+++ Models/LeThanhNamDb.cs
@@ -0,0 +1,11 @@
+namespace LeThanhNam_03.Models
+{
+    public class LeThanhNamDb 
+    {
+        public string ID { get; set; }
+        
+        public string Name { get; set; }
+        
+        
+    }
+}
